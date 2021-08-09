c4d776225940342c655e1fe318e7339874641b55
yagel elkayam <yagel8877@gmail.com>
2021-08-09 16:21:11 -0700

    responsive-nav

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 const Layout = ({ children }) => {
     
     return (
-        <div id="mynav" className='bg-gradient-to-r from-blue-300 to-green-300'>
+        <div  className='bg-gradient-to-r from-blue-300 to-green-300'>
                 
                 <Navbar />
                 <main className='pt-10'>{ children }</main>
