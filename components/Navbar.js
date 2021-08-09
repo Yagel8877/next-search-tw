b2dc1225abc98cda18f2459db90058f1f12772d7
yagel elkayam <yagel8877@gmail.com>
2021-08-09 15:33:09 -0700

    v2

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+const navbar = () => {
+    return ( 
+        <nav className="top-0 inset-x-0 fixed pb-4 border-solid text-white left-0">
+            <ul className="flex justify-between gap-3 text-xl">
+                <li className='border-2 border-black rounded-md p-1'><Link href='/' ><a>Home</a></Link></li>
+                <li className='border-2 border-black rounded-md p-1'><Link href='/page2' ><a>page2</a></Link></li>
+                <li className='border-2 border-black rounded-md p-1'><Link href='/page3' ><a>page3</a></Link></li>
+            </ul>
+        </nav>
+
+
+     );
+}
+ 
+export default navbar;
